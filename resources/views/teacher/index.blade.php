cbbd4f76bc40fd0259f3a87c044ac54b6d629e55
Azizbek Isroilov <aziz16110904@gmail.com>
2025-03-19 17:09:14 +0500

    fixed layout

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -143,13 +143,12 @@
                                                     d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                                             </svg>
                                         </div>
-                                        />
                                         </svg>
                                     </div>
-                                </div>
-                                <div>
-                                    <p class="text-sm font-medium">5 ta yangi talaba ro'yxatdan o'tdi</p>
-                                    <p class="text-xs text-gray-500">2 kun oldin, 09:15</p>
+                                    <div>
+                                        <p class="text-sm font-medium">5 ta yangi talaba ro'yxatdan o'tdi</p>
+                                        <p class="text-xs text-gray-500">2 kun oldin, 09:15</p>
+                                    </div>
                                 </div>
                             </div>
 
