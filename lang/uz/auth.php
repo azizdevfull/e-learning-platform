757994b364b5d6468c16ce4d86c06d8173b7b64c
Azizbek Isroilov <aziz16110904@gmail.com>
2025-03-20 19:54:25 +0500

    added uz language

---
+++ lang/uz/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ushbu kirish ma\'lumotlari bizning ma\'lumotlarimiz bilan mos kelmaydi.',
+    'password' => 'Kiritilgan parol noto\'g\'ri.',
+    'throttle' => ':seconds soniya dan so\'ng qaytadan urinib ko\'ring.',
+
+];
