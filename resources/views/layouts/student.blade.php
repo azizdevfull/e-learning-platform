9a174a1b71c0b0c9de3c4542c620c50b914286f6
Azizbek Isroilov <aziz16110904@gmail.com>
2025-03-20 11:40:08 +0500

    removed search and notify

--- resources/views/layouts/student.blade.php
+++ resources/views/layouts/student.blade.php
@@ -87,22 +87,6 @@
             </div>
 
             <div class="flex items-center space-x-2">
-                <div class="hidden md:block relative w-64">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="absolute left-2 top-2.5 h-4 w-4 text-gray-400"
-                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                    </svg>
-                    <input type="text" placeholder="Qidirish..."
-                        class="pl-8 pr-4 py-2 w-full rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent">
-                </div>
-                <button id="notification-btn" class="p-2 rounded-md hover:bg-gray-100">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                    </svg>
-                </button>
                 <div class="relative">
                     <button id="profile-menu-btn" class="p-1 rounded-full hover:bg-gray-100">
                         <img src="{{ asset('images/default-profile.png') }}" alt="Profile" class="h-8 w-8 rounded-full">
